ee2532ba1389d09b82885664d76d41337ba7248c
Tyler Smith <tysmith@ea.com>
2023-08-27 10:49:04 -0700

    Add single static list item to body comp log

---
+++ src/routes/bodycomp/+layout.svelte
@@ -0,0 +1,15 @@
+<slot />
+<nav class="bottom-nav"></nav>
+
+<style>
+  .bottom-nav {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+
+    height: 5rem;
+
+    background-color: red;
+  }
+</style>
---
+++ src/routes/bodycomp/log/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+  import BodyCompListItem from '$lib/components/body-comp-list-item.svelte';
+</script>
+
+<BodyCompListItem />
