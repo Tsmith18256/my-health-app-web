64da5368e91aa92b256f59f938b5738532b47ccf
Tyler Smith <tysmith@ea.com>
2023-09-06 22:16:01 -0700

    Add desktop layout to body comp list item

--- src/lib/components/body-comp-list-item/body-comp-list-item.svelte
+++ src/lib/components/body-comp-list-item/body-comp-list-item.svelte
@@ -5,33 +5,77 @@
 </script>
 
 <div class="item-container">
-	<strong>{date}</strong>
+	<span class="date-label">{date}</span>
 
-	<div class="right-side-container">
-		<span>{weight} lbs</span>
-		{#if bodyFat}
-			<span>13.9% BF</span>
-		{/if}
-	</div>
+	{#if bodyFat}
+		<span class="body-fat-label">13.9% BF</span>
+	{/if}
+	<span class="weight-label">{weight} lbs</span>
 </div>
 
 <style>
 	.item-container {
-		display: flex;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: auto;
 		align-items: center;
-		justify-content: space-between;
 
-		height: 5rem;
-		padding: 0 1rem;
+		height: 4.5rem;
+		padding: 0.9rem 1rem;
 
 		border-bottom: 1px solid #ddd;
+
+		/* @media (min-width: 481px) {
+
+		}
+
+		@media (min-width: 769px) {
+		} */
+
+		@media (min-width: 1025px) {
+			display: flex;
+			justify-content: space-between;
+
+			height: 3rem;
+		}
+
+		/* @media (min-width: 1201px) {
+		} */
 	}
 
-	.right-side-container {
-		display: flex;
-		flex-direction: column;
-		align-items: end;
-		justify-content: space-between;
-    gap: 0.25rem;
+	.date-label {
+		grid-row-start: 1;
+		grid-row-end: span 2;
+		font-size: 1.5rem;
+
+		@media (min-width: 1025px) {
+			font-weight: normal;
+			font-size: 1rem;
+		}
+	}
+
+	.weight-label {
+		grid-row-start: 1;
+		grid-row-end: span 1;
+		justify-self: end;
+
+		font-weight: bold;
+		font-size: 1.3rem;
+
+		@media (min-width: 1025px) {
+			font-size: 1rem;
+		}
+	}
+
+	.body-fat-label {
+		grid-row-start: 2;
+		grid-row-end: span 1;
+		justify-self: end;
+
+		font-size: 0.9rem;
+
+		@media (min-width: 1025px) {
+			font-size: 1rem;
+		}
 	}
 </style>
