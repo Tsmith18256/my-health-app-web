55d1fa37290d0a763e26dafe192a9c0ee46eb395
Tyler Smith <tysmith@ea.com>
2023-10-13 15:49:30 -0700

    Add edit body comp entry modal

--- src/lib/stores/body-comp/body-comp-entries/body-comp-entries.store.ts
+++ src/lib/stores/body-comp/body-comp-entries/body-comp-entries.store.ts
@@ -1,11 +1,44 @@
-import type { IBodyCompEntry } from '$lib/types/body-comp/body-comp-entry.types';
+import type { IBodyCompEntry, INewBodyCompEntry } from '$lib/types/body-comp/body-comp-entry.types';
+import { sortBodyCompEntriesByNewest } from '$lib/utils/body-comp/sort-body-comp-entries/sort-body-comp-entries.util';
+import dayjs from 'dayjs';
 import { writable } from 'svelte/store';
 
+/**
+ * A store containing all the user's body composition entries, sorted by date.
+ */
 export const bodyCompEntries = writable<IBodyCompEntry[]>([]);
 
-export const addBodyCompEntry = (newEntry: IBodyCompEntry) => {
+/**
+ * Adds the given entry to the store. An ID will be generated for the new entry.
+ */
+export const addBodyCompEntry = (newEntry: INewBodyCompEntry) => {
   bodyCompEntries.update(entries => {
-    console.log(entries.concat(newEntry));
-    return entries.concat(newEntry);
+    const entryWithId: IBodyCompEntry = {
+      ...newEntry,
+      id: dayjs().unix()
+    };
+    const entriesWithNew = entries.concat(entryWithId);
+
+    return sortBodyCompEntriesByNewest(entriesWithNew);
+  });
+};
+
+/**
+ * Updates the given entry in the store. If no entry with the given ID exists, nothing will be updated.
+ */
+export const updateBodyCompEntry = (updatedEntry: IBodyCompEntry) => {
+  let requiresResort = false;
+
+  bodyCompEntries.update(entries => {
+    const updated = entries.map(entry => {
+      if (entry.id === updatedEntry.id) {
+        requiresResort = !entry.date.isSame(updatedEntry.date);
+        return updatedEntry;
+      } else {
+        return entry;
+      }
+    });
+
+    return requiresResort ? sortBodyCompEntriesByNewest(updated) : updated;
   });
 };
