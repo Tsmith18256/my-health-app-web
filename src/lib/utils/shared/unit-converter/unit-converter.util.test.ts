9dc3f636fc31749caff981fe721cdc2cfd509e78
Tyler Smith <tysmith@ea.com>
2024-07-05 12:56:20 -0700

    Add more conversions to unit converter util

--- src/lib/utils/shared/unit-converter/unit-converter.util.test.ts
+++ src/lib/utils/shared/unit-converter/unit-converter.util.test.ts
@@ -1,88 +1,152 @@
-import { convertCmsToIns, convertGsToLbs, convertInsToCms, convertInsToMms, convertLbsToGs, convertMmsToCms, convertMmsToIns } from '$lib/utils/shared/unit-converter/unit-converter.util';
+import {
+  convertCmsToIns,
+  convertGsToKgs,
+  convertGsToLbs,
+  convertGsToOzs,
+  convertInsToCms,
+  convertInsToMms,
+  convertKgsToGs,
+  convertLbsToGs,
+  convertMmsToCms,
+  convertMmsToIns,
+  convertOzsToGs,
+} from '$lib/utils/shared/unit-converter/unit-converter.util';
 import { describe, expect, test } from 'vitest';
 
 describe('Unit Converter util', () => {
-  test('converts positive lbs to grams correctly', () => {
-    const received = convertLbsToGs(123.4);
+  describe('Weight conversions', () => {
+    test('converts positive gs to kgs correctly', () => {
+      const received = convertGsToKgs(967458);
 
-    expect(received).toBeCloseTo(55973.2985, 4);
+      expect(received).toBeCloseTo(967.458, 4);
+    });
+
+    test('converts negative gs to kgs correctly', () => {
+      const received = convertGsToKgs(-967458);
+
+      expect(received).toBeCloseTo(-967.458, 4);
+    });
+
+    test('converts positive kgs to gs correctly', () => {
+      const received = convertKgsToGs(55.6);
+
+      expect(received).toBeCloseTo(55600);
+    });
+
+    test('converts negative kgs to gs correctly', () => {
+      const received = convertKgsToGs(-55.6);
+
+      expect(received).toBeCloseTo(-55600);
+    });
+
+    test('converts positive lbs to gs correctly', () => {
+      const received = convertLbsToGs(123.4);
+
+      expect(received).toBeCloseTo(55973.2985, 4);
+    });
+
+    test('converts negative lbs to gs correctly', () => {
+      const received = convertLbsToGs(-123.4);
+
+      expect(received).toBeCloseTo(-55973.2985, 4);
+    });
+
+    test('converts positive ozs to gs correctly', () => {
+      const received = convertOzsToGs(14.3);
+
+      expect(received).toBeCloseTo(405.3981);
+    });
+
+    test('converts negative ozs to gs correctly', () => {
+      const received = convertOzsToGs(-14.3);
+
+      expect(received).toBeCloseTo(-405.3981);
+    });
+
+    test('converts positive gs to lbs correctly', () => {
+      const received = convertGsToLbs(1500);
+
+      expect(received).toBeCloseTo(3.3069, 4);
+    });
+
+    test('converts negative lbs to gs correctly', () => {
+      const received = convertGsToLbs(-1500);
+
+      expect(received).toBeCloseTo(-3.3069, 4);
+    });
+
+    test('converts positive gs to ozs correctly', () => {
+      const received = convertGsToOzs(356);
+
+      expect(received).toBeCloseTo(12.5575);
+    });
+
+    test('converts negative gs to ozs correctly', () => {
+      const received = convertGsToOzs(-356);
+
+      expect(received).toBeCloseTo(-12.5575);
+    });
   });
 
-  test('converts negative lbs to grams correctly', () => {
-    const received = convertLbsToGs(-123.4);
+  describe('Distance conversions', () => {
+    test('converts positive mms to cms correctly', () => {
+      const received = convertMmsToCms(1743);
 
-    expect(received).toBeCloseTo(-55973.2985, 4);
-  });
+      expect(received).toBeCloseTo(174.3, 4);
+    });
 
-  test('converts positive grams to lbs correctly', () => {
-    const received = convertGsToLbs(1500);
+    test('converts negative mms to cms correctly', () => {
+      const received = convertMmsToCms(-1743);
 
-    expect(received).toBeCloseTo(3.3069, 4);
-  });
+      expect(received).toBeCloseTo(-174.3, 4);
+    });
 
-  test('converts negative lbs to grams correctly', () => {
-    const received = convertGsToLbs(-1500);
+    test('converts positive ins to cms correctly', () => {
+      const received = convertInsToCms(12);
 
-    expect(received).toBeCloseTo(-3.3069, 4);
-  });
+      expect(received).toBeCloseTo(30.48, 4);
+    });
 
-  test('converts positive mms to cms correctly', () => {
-    const received = convertMmsToCms(1743);
+    test('converts negative ins to cms correctly', () => {
+      const received = convertInsToCms(-12);
 
-    expect(received).toBeCloseTo(174.3, 4);
-  });
+      expect(received).toBeCloseTo(-30.48, 4);
+    });
 
-  test('converts negative mms to cms correctly', () => {
-    const received = convertMmsToCms(-1743);
+    test('converts positive ins to mms correctly', () => {
+      const received = convertInsToMms(6);
 
-    expect(received).toBeCloseTo(-174.3, 4);
-  });
+      expect(received).toBeCloseTo(152.4, 4);
+    });
 
-  test('converts positive ins to cms correctly', () => {
-    const received = convertInsToCms(12);
+    test('converts negative ins to mms correctly', () => {
+      const received = convertInsToMms(-6);
 
-    expect(received).toBeCloseTo(30.48, 4);
-  });
+      expect(received).toBeCloseTo(-152.4, 4);
+    });
 
-  test('converts negative ins to cms correctly', () => {
-    const received = convertInsToCms(-12);
+    test('converts positive mms to ins correctly', () => {
+      const received = convertMmsToIns(37);
 
-    expect(received).toBeCloseTo(-30.48, 4);
-  });
+      expect(received).toBeCloseTo(1.4567, 4);
+    });
 
-  test('converts positive ins to mms correctly', () => {
-    const received = convertInsToMms(6);
+    test('converts negative mms to ins correctly', () => {
+      const received = convertMmsToIns(-37);
 
-    expect(received).toBeCloseTo(152.4, 4);
-  });
+      expect(received).toBeCloseTo(-1.4567, 4);
+    });
 
-  test('converts negative ins to mms correctly', () => {
-    const received = convertInsToMms(-6);
+    test('converts positive cms to ins correctly', () => {
+      const received = convertCmsToIns(13);
 
-    expect(received).toBeCloseTo(-152.4, 4);
-  });
+      expect(received).toBeCloseTo(5.1181, 4);
+    });
 
-  test('converts positive mms to ins correctly', () => {
-    const received = convertMmsToIns(37);
+    test('converts negative cms to ins correctly', () => {
+      const received = convertCmsToIns(-13);
 
-    expect(received).toBeCloseTo(1.4567, 4);
-  });
-
-  test('converts negative mms to ins correctly', () => {
-    const received = convertMmsToIns(-37);
-
-    expect(received).toBeCloseTo(-1.4567, 4);
-  });
-
-  test('converts positive cms to ins correctly', () => {
-    const received = convertCmsToIns(13);
-
-    expect(received).toBeCloseTo(5.1181, 4);
-  });
-
-  test('converts negative cms to ins correctly', () => {
-    const received = convertCmsToIns(-13);
-
-    expect(received).toBeCloseTo(-5.1181, 4);
+      expect(received).toBeCloseTo(-5.1181, 4);
+    });
   });
 });
--- src/lib/utils/shared/unit-converter/unit-converter.util.ts
+++ src/lib/utils/shared/unit-converter/unit-converter.util.ts
@@ -3,6 +3,7 @@
 // ------------------
 // Imperial/metric conversions.
 export const LB_PER_KG = 2.2046226218;
+export const G_PER_OZ = 28.34952;
 // Metric conversions.
 export const G_PER_KG = 1000;
 
@@ -18,16 +19,34 @@ export const MM_PER_CM = 10;
 // ----- WEIGHT -----
 // ------------------
 
+// Metric
+
+export const convertGsToKgs = (gs: number): number => {
+  return gs / G_PER_KG;
+};
+
+export const convertKgsToGs = (kgs: number): number => {
+  return kgs * G_PER_KG;
+};
+
 // Imperial -> Metric
 
 export const convertLbsToGs = (lbs: number): number => {
   return (lbs / LB_PER_KG) * G_PER_KG;
 };
 
+export const convertOzsToGs = (ozs: number): number => {
+  return ozs * G_PER_OZ;
+};
+
 // Metric -> Imperial
 
 export const convertGsToLbs = (gs: number): number => {
-  return gs / G_PER_KG * LB_PER_KG;
+  return (gs / G_PER_KG) * LB_PER_KG;
+};
+
+export const convertGsToOzs = (gs: number): number => {
+  return gs / G_PER_OZ;
 };
 
 // --------------------
