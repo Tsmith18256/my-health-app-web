4185e7e6bf81315fe0fb5bd7f48daca5868eca70
Tyler Smith <tysmith@ea.com>
2024-08-10 12:11:42 -0700

    Add bottom bar

---
+++ src/lib/root/components/bottom-bar/bottom-bar.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import { TabAnchor, TabGroup } from '@skeletonlabs/skeleton';
+  import { page } from '$app/stores';
+</script>
+
+<nav class="block md:hidden">
+  <TabGroup
+    class="bg-surface-100-800-token"
+    justify="justify-center"
+    active="variant-filled-primary"
+    hover="hover:variant-soft-primary"
+    flex="flex-1 lg:flex-none"
+    rounded=""
+    border=""
+    >
+    <TabAnchor
+      href="/exercise"
+      selected={$page.url.pathname.startsWith('/exercise')}
+    >
+      <!-- TODO: Update all FA icons to a common component -->
+      <svelte:fragment slot="lead"
+        ><i class="far fa-running" /></svelte:fragment
+      >
+      Exercise
+    </TabAnchor>
+    <TabAnchor
+      href="/nutrition"
+      selected={$page.url.pathname.startsWith('/nutrition')}
+    >
+      <svelte:fragment slot="lead"
+        ><i class="far fa-apple-alt" /></svelte:fragment
+      >
+      Nutrition
+    </TabAnchor>
+    <TabAnchor
+      href="/bodycomp/log"
+      selected={$page.url.pathname.startsWith('/bodycomp')}
+    >
+      <svelte:fragment slot="lead"
+        ><i class="far fa-heartbeat" /></svelte:fragment
+      >
+      Body Comp
+    </TabAnchor>
+  </TabGroup>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
   import {
     Modal,
-    TabGroup,
-    TabAnchor,
     initializeStores,
     type ModalComponent,
   } from '@skeletonlabs/skeleton';
   import { inject } from '@vercel/analytics';
   import { dev } from '$app/environment';
-  import { page } from '$app/stores';
   import BodyCompEditEntryModal from '$lib/body-comp/components/body-comp-edit-entry-modal/body-comp-edit-entry-modal.svelte';
+  import BottomBar from '$lib/root/components/bottom-bar/bottom-bar.svelte';
+  import TopBar from '$lib/root/components/top-bar/top-bar.svelte';
   import '$lib/shared/assets/fonts/css/fontawesome.min.css';
   import '$lib/shared/assets/fonts/css/regular.min.css';
   import '$lib/shared/polyfills';
   import '../app.css';
-    import TopBar from '$lib/root/components/top-bar.svelte';
 
   initializeStores();
 
@@ -32,44 +30,5 @@
 
   <slot />
 
-  <nav>
-    <TabGroup
-      justify="justify-center"
-      active="variant-filled-primary"
-      hover="hover:variant-soft-primary"
-      flex="flex-1 lg:flex-none"
-      rounded=""
-      border=""
-      class="bg-surface-100-800-token w-full"
-    >
-      <TabAnchor
-        href="/exercise"
-        selected={$page.url.pathname.startsWith('/exercise')}
-      >
-        <!-- TODO: Update all FA icons to a common component -->
-        <svelte:fragment slot="lead"
-          ><i class="far fa-running" /></svelte:fragment
-        >
-        Exercise
-      </TabAnchor>
-      <TabAnchor
-        href="/nutrition"
-        selected={$page.url.pathname.startsWith('/nutrition')}
-      >
-        <svelte:fragment slot="lead"
-          ><i class="far fa-apple-alt" /></svelte:fragment
-        >
-        Nutrition
-      </TabAnchor>
-      <TabAnchor
-        href="/bodycomp/log"
-        selected={$page.url.pathname.startsWith('/bodycomp')}
-      >
-        <svelte:fragment slot="lead"
-          ><i class="far fa-heartbeat" /></svelte:fragment
-        >
-        Body Comp
-      </TabAnchor>
-    </TabGroup>
-  </nav>
+  <BottomBar />
 </div>
