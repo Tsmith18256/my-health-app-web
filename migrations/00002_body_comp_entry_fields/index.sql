251a2c1c268b67a6c32880326144b3125bf86a7f
Tyler Smith <tysmith@ea.com>
2024-09-02 19:08:19 -0700

    Add second database migration

---
+++ migrations/00002_body_comp_entry_fields/index.sql
@@ -0,0 +1,5 @@
+ALTER TABLE body_comp_entries ADD COLUMN waist_circ_in_mm INTEGER;
+ALTER TABLE body_comp_entries ADD COLUMN neck_circ_in_mm INTEGER;
+ALTER TABLE body_comp_entries ADD COLUMN chest_skinfold INTEGER;
+ALTER TABLE body_comp_entries ADD COLUMN ab_skinfold INTEGER;
+ALTER TABLE body_comp_entries ADD COLUMN thigh_skinfold INTEGER;
