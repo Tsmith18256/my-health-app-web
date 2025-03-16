eddedc35e4962ac9b5b0c1a5a323b73295238dc3
Tyler Smith <tysmith@ea.com>
2025-03-16 10:39:52 -0700

    Format code

--- src/app/(app)/body-comp/log/page.tsx
+++ src/app/(app)/body-comp/log/page.tsx
@@ -1,13 +1,16 @@
-import { BodyCompRow, IBodyCompRowProps } from '@/app/(app)/body-comp/log/body-comp-row.component';
-import { FloatingActionButton } from '@/app/(app)/body-comp/log/floating-action-button.component';
-import dayjs from 'dayjs';
+import {
+  BodyCompRow,
+  IBodyCompRowProps,
+} from "@/app/(app)/body-comp/log/body-comp-row.component";
+import { FloatingActionButton } from "@/app/(app)/body-comp/log/floating-action-button.component";
+import dayjs from "dayjs";
 
 const entries: IBodyCompRowProps[] = [];
 for (let i = 0; i < 20; i++) {
   entries.push({
-    date: dayjs().subtract(i, 'day').format('MMMM D, YYYY'),
+    date: dayjs().subtract(i, "day").format("MMMM D, YYYY"),
     weight: `${(Math.random() * 5 + 172.5).toFixed(1)} lbs`,
-    bodyFat: `${(Math.random() + 15.2).toFixed(1)}%`
+    bodyFat: `${(Math.random() + 15.2).toFixed(1)}%`,
   });
 }
 
