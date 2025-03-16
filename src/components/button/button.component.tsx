f0cd9ba7c8e06dd0ff8625f6f2ffd3799ab7fa7d
Tyler Smith <tysmith@ea.com>
2025-03-16 09:37:55 -0700

    Add colors to wireframe

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,14 +1,33 @@
-import { ComponentProps } from 'react';
-
+import { ObjectValues } from "@/types/ObjectValues";
+import { ComponentProps } from "react";
 
 export const Button = (props: IButtonProps) => {
+  const size = props.size ?? ButtonSize.Medium;
+
   return (
-    <button className='bg-gray-700 p-4 w-full'>
+    <button
+      className={`bg-orange-400 border-3 font-semibold rounded-lg text-black w-full ${
+        classesBySize[size]
+      }`}
+    >
       {props.children}
     </button>
   );
 };
 
-export interface IButtonProps {
-  children: ComponentProps<'button'>['children'];
+export const ButtonSize = {
+  Medium: "Medium",
+  Large: "Large",
+} as const;
+
+export type ButtonSize = ObjectValues<typeof ButtonSize>;
+
+const classesBySize: Record<ButtonSize, string> = {
+  [ButtonSize.Medium]: "p-3 text-lg",
+  [ButtonSize.Large]: "p-4 text-2xl",
+};
+
+export interface IButtonProps
+  extends Pick<ComponentProps<"button">, "children"> {
+  size?: ButtonSize;
 }
