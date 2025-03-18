8deb7adf29fbd16be60c7e5aef822b114a149762
Tyler Smith <tysmith@ea.com>
2025-03-18 13:05:35 -0700

    Finish new body comp entry page

--- src/app/(app)/body-comp/new/page.tsx
+++ src/app/(app)/body-comp/new/page.tsx
@@ -5,33 +5,35 @@ import Link from "next/link";
 
 export default function NewBodyCompEntryPage() {
   return (
-    <div className="px-4 py-6">
-      <header>
+    <>
+      <header className="pt-6 px-4">
         <Heading level={HeadingLevel.h1}>New entry</Heading>
       </header>
 
-      <form className="flex flex-col gap-6 mt-6">
-        <Input id="txtDate" label="Date" />
-        <Input id="txtWeight" label="Weight" />
-
-        <section className="flex flex-col gap-6">
-          <Heading level={HeadingLevel.h2}>Advanced</Heading>
+      <form>
+        <main className="flex flex-col gap-6 mt-6 pb-4 px-4">
+          <Input id="txtDate" label="Date" />
+          <Input id="txtWeight" label="Weight" />
 
           <section className="flex flex-col gap-6">
-            <Heading level={HeadingLevel.h3}>Measuring tape</Heading>
-            <Input id="txtNeckCirc" label="Neck" />
-            <Input id="txtWaistCirc" label="Waist" />
-          </section>
+            <Heading level={HeadingLevel.h2}>Advanced</Heading>
 
-          <section className="flex flex-col gap-6">
-            <Heading level={HeadingLevel.h3}>Calipers (skinfold)</Heading>
-            <Input id="txtChestSkinfold" label="Chest" />
-            <Input id="txtAbSkinfold" label="Abdominal" />
-            <Input id="txtThighSkinfold" label="Thigh" />
-          </section>
-        </section>
+            <section className="flex flex-col gap-6">
+              <Heading level={HeadingLevel.h3}>Measuring tape</Heading>
+              <Input id="txtNeckCirc" label="Neck" />
+              <Input id="txtWaistCirc" label="Waist" />
+            </section>
 
-        <footer className="flex gap-3 justify-stretch w-full">
+            <section className="flex flex-col gap-6">
+              <Heading level={HeadingLevel.h3}>Calipers (skinfold)</Heading>
+              <Input id="txtChestSkinfold" label="Chest" />
+              <Input id="txtAbSkinfold" label="Abdominal" />
+              <Input id="txtThighSkinfold" label="Thigh" />
+            </section>
+          </section>
+        </main>
+
+        <footer className="bg-(--background) border-t border-t-gray-400 bottom-0 flex gap-3 inset-x-0 justify-stretch px-4 py-6 sticky">
           <Link className="grow" href="/body-comp/log">
             <Button>Create</Button>
           </Link>
@@ -41,6 +43,6 @@ export default function NewBodyCompEntryPage() {
           </Link>
         </footer>
       </form>
-    </div>
+    </>
   );
 }
