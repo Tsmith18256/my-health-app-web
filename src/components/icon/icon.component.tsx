89e8b26b3e1c310b84a32ab5c42a173cb2b8c4df
Tyler Smith <tysmith@ea.com>
2025-03-18 22:07:22 -0700

    Add plus icon to FAB

---
+++ src/components/icon/icon.component.tsx
@@ -0,0 +1,17 @@
+import { ObjectValues } from '@/types/ObjectValues';
+
+export const Icon = (props: IIconProps) => {
+  return (
+    <i className={`far ${props.icon}`} />
+  );
+}
+
+export const IconImage = {
+  Plus: "fa-plus"
+} as const;
+
+export type IconImage = ObjectValues<typeof IconImage>;
+
+export interface IIconProps {
+  icon: IconImage;
+}
