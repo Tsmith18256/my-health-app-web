6bb286dd95b498a5fc609d5303dc8d5b06d80c60
Tyler Smith <tysmith@ea.com>
2025-03-19 11:29:29 -0700

    Populate edit entry form on load

--- src/app/(app)/body-comp/body-comp-entry-form.component.tsx
+++ src/app/(app)/body-comp/body-comp-entry-form.component.tsx
@@ -6,7 +6,11 @@ import {
 } from "@/components/button/button.component";
 import { Header } from "@/components/header/header.component";
 import { Heading, HeadingLevel } from "@/components/heading/heading.component";
-import { Icon, IconImage } from '@/components/icon/icon.component';
+import { Icon, IconImage } from "@/components/icon/icon.component";
+import {
+  BodyCompEntryId,
+  selectBodyCompEntryById,
+} from "@/database/models/body-comp-entry.model";
 import Link from "next/link";
 
 export const BodyCompEntryForm = (props: IBodyCompEntryFormProps) => {
@@ -24,40 +28,62 @@ export const BodyCompEntryForm = (props: IBodyCompEntryFormProps) => {
     </Link>
   ) : undefined;
 
+  const entry = props.entryId && selectBodyCompEntryById(props.entryId);
+
   return (
     <>
       <Header endContent={headerEndContent} title={title} />
 
       <form>
         <main className="flex flex-col gap-6 mt-6 pb-4 px-4">
-          <Input id="txtDate" label="Date" />
-          <Input id="txtWeight" label="Weight" step="0.1" type="number" />
+          <Input
+            id="txtDate"
+            defaultValue={entry?.date.format("YYYY-MM-DD")}
+            label="Date"
+            type="date"
+          />
+          <Input
+            id="txtWeight"
+            defaultValue={entry?.weight.toFixed(1)}
+            label="Weight"
+            step="0.1"
+            type="number"
+          />
 
           <section className="flex flex-col gap-6">
             <Heading level={HeadingLevel.h2}>Advanced</Heading>
 
             <section className="flex flex-col gap-6">
               <Heading level={HeadingLevel.h3}>Measuring tape</Heading>
-              <Input id="txtNeckCirc" label="Neck" step="0.1" type="number" />
-              <Input id="txtWaistCirc" label="Waist" step="0.1" type="number" />
+              <Input
+                id="txtNeckCirc"
+                defaultValue={entry?.neckCircumference?.toFixed(0)}
+                label="Neck"
+                step="0.1"
+                type="number"
+              />
+              <Input id="txtWaistCirc" defaultValue={entry?.waistCircumference?.toFixed(0)} label="Waist" step="0.1" type="number" />
             </section>
 
             <section className="flex flex-col gap-6">
               <Heading level={HeadingLevel.h3}>Calipers (skinfold)</Heading>
               <Input
                 id="txtChestSkinfold"
+                defaultValue={entry?.chestSkinfold?.toFixed(0)}
                 label="Chest"
                 step="1"
                 type="number"
               />
               <Input
                 id="txtAbSkinfold"
+                defaultValue={entry?.abSkinfold?.toFixed(0)}
                 label="Abdominal"
                 step="1"
                 type="number"
               />
               <Input
                 id="txtThighSkinfold"
+                defaultValue={entry?.thighSkinfold?.toFixed(0)}
                 label="Thigh"
                 step="1"
                 type="number"
@@ -80,6 +106,9 @@ export const BodyCompEntryForm = (props: IBodyCompEntryFormProps) => {
   );
 };
 
-export interface IBodyCompEntryFormProps {
-  isEditMode?: boolean;
-}
+export type IBodyCompEntryFormProps =
+  | {
+      isEditMode: true;
+      entryId: BodyCompEntryId;
+    }
+  | { isEditMode?: false; entryId?: undefined };
--- src/app/(app)/body-comp/edit/[entry-id]/page.tsx
+++ src/app/(app)/body-comp/edit/[entry-id]/page.tsx
@@ -1,5 +1,6 @@
-import { BodyCompEntryForm } from '@/app/(app)/body-comp/body-comp-entry-form.component';
+import { BodyCompEntryForm } from "@/app/(app)/body-comp/body-comp-entry-form.component";
+import { BodyCompEntryId } from "@/database/models/body-comp-entry.model";
 
 export default function EditBodyCompEntryPage() {
-  return <BodyCompEntryForm isEditMode={true} />;
+  return <BodyCompEntryForm isEditMode={true} entryId={6 as BodyCompEntryId} />;
 }
