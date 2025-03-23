f68bbb0c084383aa3bf45e09fe32ffaa3e3e2f2a
Tyler Smith <tysmith@ea.com>
2025-03-23 11:23:12 -0700

    Add DATABASE_URL env variable

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,5 +1,8 @@
-import postgres from 'postgres';
+import postgres from "postgres";
 
-export const sql = postgres(
-  'postgres://postgres:postgres@localhost:5432/myhealthapp',
-);
+const isDev = process.env.NODE_ENV === "development";
+const databaseUrl = isDev
+  ? "postgres://postgres:postgres@localhost:5432/myhealthapp"
+  : process.env.DATABASE_URL ?? '';
+
+export const sql = postgres(databaseUrl, { ssl: isDev ? undefined : "verify-full" });
