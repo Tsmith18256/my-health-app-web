0158ac550e1e92a50fdfbeb2fa6aa5d688c6a6ed
Tyler Smith <tysmith@ea.com>
2025-05-24 20:30:58 -0700

    Move FAB to body comp with nav layout

--- src/app/(app)/body-comp/(with-nav)/layout.tsx
+++ src/app/(app)/body-comp/(with-nav)/layout.tsx
@@ -1,12 +1,19 @@
 import { BodyCompNav } from '@/features/body-comp/nav/body-comp-nav.component';
+import { FloatingActionButton } from '@/shared/components/buttons/floating-action-button/floating-action-button.component';
 import { ILayoutProps } from '@/shared/helper-types/layout-props.type';
+import Link from 'next/link';
+import styles from './body-comp-with-nav-layout.module.css';
 
 export default function BodyCompNavLayout({ children }: ILayoutProps) {
   return (
-    <>
+    <div className={styles.container}>
       <BodyCompNav />
 
       {children}
-    </>
+
+      <Link href="/body-comp/new">
+        <FloatingActionButton />
+      </Link>
+    </div>
   );
 }
--- src/app/(app)/body-comp/(with-nav)/log/page.tsx
+++ src/app/(app)/body-comp/(with-nav)/log/page.tsx
@@ -1,22 +1,13 @@
-import { BodyCompLogList } from '@/features/body-comp/log/list/body-comp-log-list.component';
-import { FloatingActionButton } from "@/shared/components/buttons/floating-action-button/floating-action-button.component";
-import { LoadingIndicator } from '@/shared/components/loading-indicator/loading-indicator.component';
-import Link from "next/link";
-import { Suspense } from 'react';
-import styles from './body-comp-log-page.module.css';
+import { BodyCompLogList } from "@/features/body-comp/log/list/body-comp-log-list.component";
+import { LoadingIndicator } from "@/shared/components/loading-indicator/loading-indicator.component";
+import { Suspense } from "react";
 
 export default function BodyCompLogPage() {
   return (
-    <>
-      <main className={styles.container}>
-        <Suspense fallback={<LoadingIndicator />}>
-          <BodyCompLogList />
-        </Suspense>
-      </main>
-
-      <Link href="/body-comp/new">
-        <FloatingActionButton />
-      </Link>
-    </>
+    <main>
+      <Suspense fallback={<LoadingIndicator />}>
+        <BodyCompLogList />
+      </Suspense>
+    </main>
   );
 }
